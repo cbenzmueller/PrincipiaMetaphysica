7ddbc7afd805d8038df4bb6611dfee8b7789a997
Benzmüller <cbenzmueller@ontoleo.speedport_w_724v_09011603_00_012>
2015-11-08 06:41:29 +0100

    free logic and update

---
+++ FreeLogic/FreeHOL.thy
@@ -0,0 +1,36 @@
+(*<*) 
+theory FreeHOL   
+imports Main 
+
+begin
+(*>*)
+
+  typedecl \<iota>                -- "the type for indiviuals"   
+  type_synonym \<sigma> = "bool"   -- "the type for Booleans"   
+
+  consts  f_r  :: "\<iota> \<Rightarrow> \<iota> \<Rightarrow> \<sigma>" (*<*)(infixr "\<^bold>r" 70)(*>*) 
+   
+  consts f_e :: "'a \<Rightarrow> \<sigma>" (*<*)("\<^bold>e")(*>*)
+
+ 
+
+abbreviation f_not :: "\<sigma> \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<not>")(*>*) 
+  where "\<^bold>\<not> \<phi> \<equiv> \<not> \<phi>"    
+abbreviation f_and :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (*<*)(infixr "\<^bold>\<and>" 52)(*>*) 
+  where "\<phi> \<^bold>\<and> \<psi> \<equiv> \<phi> \<and> \<psi>"   
+abbreviation f_or :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (*<*)(infixr "\<^bold>\<or>" 51)(*>*) 
+  where "\<phi> \<^bold>\<or> \<psi> \<equiv> \<phi> \<or> \<psi>"   
+abbreviation f_implies :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (*<*)(infixr "\<^bold>\<rightarrow>" 49)(*>*) 
+  where "\<phi> \<^bold>\<rightarrow> \<psi> \<equiv> \<phi> \<longrightarrow> \<psi>"  
+abbreviation f_equals :: "\<iota> \<Rightarrow> \<iota> \<Rightarrow> \<sigma>" (*<*)(infixr "\<^bold>=" 49)(*>*) 
+  where "\<phi> \<^bold>= \<psi> \<equiv> \<phi> = \<psi>"
+abbreviation f_forall :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<forall>")(*>*) 
+  where "\<^bold>\<forall> \<Phi> \<equiv> \<forall>x. \<^bold>e x \<longrightarrow>  \<Phi> x"   
+abbreviation f_mexists :: "('a \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<exists>")(*>*) 
+  where "\<^bold>\<exists> \<Phi> \<equiv> \<exists>x. \<^bold>e x \<and> \<Phi> x"
+
+
+lemma "x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x" apply simp done
+lemma "(x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x) \<^bold>\<rightarrow> \<^bold>\<exists>(\<lambda>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" nitpick oops
+lemma "\<^bold>\<exists>(\<lambda>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" nitpick oops
+lemma "((x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x) \<^bold>\<and> \<^bold>\<exists>(\<lambda>y. (y \<^bold>= y))) \<^bold>\<rightarrow> \<^bold>\<exists>(\<lambda>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" apply simp done
