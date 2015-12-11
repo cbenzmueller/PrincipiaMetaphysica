78d91a64e78ee6c645ac336bb9e265ec1d4e56f7
Benzmüller <cbenzmueller@ontoleo.local>
2015-12-11 12:34:02 +0100

    Free Logic Update

--- FreeLogic/FreeHOL.thy
+++ FreeLogic/FreeHOL.thy
@@ -15,9 +15,9 @@ consts f_e :: "\<iota> \<Rightarrow> \<sigma>" (*<*)("\<^bold>e")(*>*)
 consts f_star :: "\<iota>" (*<*)("\<^bold>\<star>")(*>*)
 
 axiomatization where f_star_axiom: "\<not> \<^bold>e \<^bold>\<star>"
-  
 
-abbreviation f_not :: "\<sigma> \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<not>")(*>*) 
+
+abbreviation f_not :: "\<sigma> \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<not> _" [58] 59)(*>*) 
   where "\<^bold>\<not> \<phi> \<equiv> \<not> \<phi>"    
 abbreviation f_and :: "\<sigma> \<Rightarrow> \<sigma> \<Rightarrow> \<sigma>" (*<*)(infixr "\<^bold>\<and>" 52)(*>*) 
   where "\<phi> \<^bold>\<and> \<psi> \<equiv> \<phi> \<and> \<psi>"   
@@ -31,24 +31,29 @@ abbreviation f_equals :: "\<iota> \<Rightarrow> \<iota> \<Rightarrow> \<sigma>" 
   where "x \<^bold>= y \<equiv> x = y"
 abbreviation f_forall :: "(\<iota> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<forall>")(*>*) 
   where "\<^bold>\<forall> \<Phi> \<equiv> \<forall>x. \<^bold>e x \<longrightarrow>  \<Phi> x"   
+abbreviation f_forall_binder :: "(\<iota> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (*<*) (binder "\<^bold>\<forall>" [8] 9) (*>*)
+  where "\<^bold>\<forall> x. \<phi> x \<equiv> \<^bold>\<forall> \<phi>"
 abbreviation f_mexists :: "(\<iota> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (*<*)("\<^bold>\<exists>")(*>*) 
   where "\<^bold>\<exists> \<Phi> \<equiv> \<exists>x. \<^bold>e x \<and> \<Phi> x"
+abbreviation f_exists_binder :: "(\<iota> \<Rightarrow> \<sigma>) \<Rightarrow> \<sigma>" (*<*) (binder "\<^bold>\<exists>" [8] 9) (*>*)
+  where "\<^bold>\<exists> x. \<phi> x \<equiv> \<^bold>\<exists> \<phi>"
 abbreviation f_that :: "(\<iota> \<Rightarrow> \<sigma>) \<Rightarrow> \<iota>" (*<*)("\<^bold>I")(*>*) 
   where "\<^bold>I \<Phi> \<equiv> if \<^bold>\<exists>(\<lambda>x. (\<Phi> x) \<and> \<^bold>\<forall>(\<lambda>y. (\<Phi> y) \<longrightarrow> (y = x))) then THE x. (\<Phi> x) else \<^bold>\<star>"
-
+abbreviation f_that_binder :: "(\<iota> \<Rightarrow> \<sigma>) \<Rightarrow> \<iota>" (*<*) (binder "\<^bold>I" [8] 9) (*>*)
+  where "\<^bold>I x. \<phi> x \<equiv> \<^bold>I \<phi>"
 
 section {* Some Introductory Tests *} 
 
 lemma "x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x" by simp
-lemma "(x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x) \<^bold>\<rightarrow> \<^bold>\<exists>(\<lambda>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" nitpick oops
-lemma "\<^bold>\<exists>(\<lambda>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" nitpick oops
-lemma "((x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x) \<^bold>\<and> \<^bold>\<exists>(\<lambda>y. (y \<^bold>= y))) \<^bold>\<rightarrow> \<^bold>\<exists>(\<lambda>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" by simp
+lemma "(x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x) \<^bold>\<rightarrow> (\<^bold>\<exists>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" nitpick oops
+lemma "\<^bold>\<exists>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y" nitpick oops
+lemma "((x \<^bold>r x \<^bold>\<rightarrow> x \<^bold>r x) \<^bold>\<and> (\<^bold>\<exists>y. y \<^bold>= y)) \<^bold>\<rightarrow> (\<^bold>\<exists>y. y \<^bold>r y \<^bold>\<rightarrow> y \<^bold>r y)" by simp
 
 
-lemma "\<^bold>\<not> (\<^bold>\<exists>(\<lambda>x. (x \<^bold>= (\<^bold>I (\<lambda>y. \<^bold>\<not> (y \<^bold>= y))))))" by (simp add: f_star_axiom) 
+lemma "\<^bold>\<not>(\<^bold>\<exists>x. (x \<^bold>= (\<^bold>Iy. \<^bold>\<not> y \<^bold>= y)))" by (simp add: f_star_axiom) 
 
 
-lemma "\<^bold>\<exists>(\<lambda>x. (x \<^bold>= a)) \<longrightarrow>  \<^bold>e a" by simp
+lemma "(\<^bold>\<exists>x. x \<^bold>= a) \<^bold>\<rightarrow>  \<^bold>e a" by simp
 
 
 section {* Some Principles from Scott 1967 *}
@@ -59,13 +64,14 @@ axiomatization where ax1: "\<^bold>e a \<^bold>\<and> \<^bold>e b \<^bold>\<and>
 
 lemma test: "\<^bold>\<star> \<^bold>= (\<^bold>I (\<lambda>x. x  \<^bold>= a \<^bold>\<or> x \<^bold>= b))" apply simp using ax1 by auto
 
-lemma I1: "\<^bold>\<forall>(\<lambda>y. (y \<^bold>= (\<^bold>I (\<lambda>x. \<phi>(x)))) \<^bold>\<leftrightarrow> \<^bold>\<forall>(\<lambda>x. (x \<^bold>= y) \<^bold>\<leftrightarrow> \<phi>(x)))" apply simp nitpick oops
+lemma I1: "\<^bold>\<forall>y. (y \<^bold>= (\<^bold>Ix. \<phi>(x)) \<^bold>\<leftrightarrow> (\<^bold>\<forall>x. (x \<^bold>= y) \<^bold>\<leftrightarrow> \<phi>(x)))" apply simp nitpick oops
 
-axiomatization where I1: "\<^bold>\<forall>(\<lambda>y. (y \<^bold>= (\<^bold>I (\<lambda>x. \<phi>(x)))) \<^bold>\<leftrightarrow> \<^bold>\<forall>(\<lambda>x. (x \<^bold>= y) \<^bold>\<leftrightarrow> \<phi>(x)))" 
+axiomatization where I1: "\<^bold>\<forall>y. (y \<^bold>= (\<^bold>Ix. \<phi>(x)) \<^bold>\<leftrightarrow> (\<^bold>\<forall>x. (x \<^bold>= y) \<^bold>\<leftrightarrow> \<phi>(x)))" 
 
-lemma I2: "\<^bold>\<not> (\<^bold>\<exists>(\<lambda>y. (y \<^bold>= (\<^bold>I (\<lambda>x. \<phi>(x)))))) \<^bold>\<rightarrow> ( \<^bold>\<star> \<^bold>= (\<^bold>I (\<lambda>x. \<phi>(x))))" apply simp nitpick oops
+lemma I2: "\<^bold>\<not>(\<^bold>\<exists>y. (y \<^bold>= (\<^bold>Ix. \<phi>(x)))) \<^bold>\<rightarrow> ( \<^bold>\<star> \<^bold>= (\<^bold>Ix. \<phi>(x)))" apply simp nitpick oops
 
-lemma ext: "\<^bold>\<forall>(\<lambda>x. \<phi>(x) \<^bold>\<leftrightarrow> \<psi>(x)) \<^bold>\<rightarrow> (\<^bold>I (\<lambda>x. \<phi>(x))) \<^bold>= (\<^bold>I (\<lambda>x. \<psi>(x)))" apply simp nitpick oops
+lemma ext: "(\<^bold>\<forall>x. \<phi>(x) \<^bold>\<leftrightarrow> \<psi>(x)) \<^bold>\<rightarrow> (\<^bold>Ix. \<phi>(x)) \<^bold>= (\<^bold>Ix. \<psi>(x))" apply simp sledgehammer
+nitpick oops
 
 
 
