ed3caa64dd0aca47cdfa58176240f8010b6e5f78
Benzmüller, Christoph <cbenzmueller@zedat.fu-berlin.de>
2015-12-16 21:58:50 -0800

    corrected the definition of definite description

--- FreeLogic/FreeHOL.thy
+++ FreeLogic/FreeHOL.thy
@@ -6,17 +6,15 @@ typedecl \<iota>                -- "the type for indiviuals"
 type_synonym \<sigma> = "bool"   -- "the type for Booleans"   
 
 consts f_r  :: "\<iota>\<Rightarrow>\<iota>\<Rightarrow>\<sigma>" (infixr "\<^bold>r" 70) 
-   
-consts f_e :: "\<iota>\<Rightarrow>\<sigma>" ("\<^bold>e")
-
+consts f_A :: "\<iota>\<Rightarrow>\<sigma>" ("\<A>")   
 consts f_star :: "\<iota>" ("\<^bold>\<star>")
 
-axiomatization where f_star_axiom: "\<not> \<^bold>e \<^bold>\<star>"
+axiomatization where f_star_axiom: "\<not> \<A> \<^bold>\<star>"
 
 
 abbreviation f_not :: "\<sigma>\<Rightarrow>\<sigma>" ("\<^bold>\<not>_" [58] 59)            where "\<^bold>\<not>\<phi> \<equiv> \<not>\<phi>"     
 abbreviation f_implies :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (infixr "\<^bold>\<rightarrow>" 49)   where "\<phi>\<^bold>\<rightarrow>\<psi> \<equiv> \<phi>\<longrightarrow>\<psi>" 
-abbreviation f_all :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" ("\<^bold>\<forall>")                 where "\<^bold>\<forall>\<Phi> \<equiv> \<forall>x. \<^bold>e(x) \<longrightarrow>  \<Phi>(x)"   
+abbreviation f_all :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" ("\<^bold>\<forall>")                 where "\<^bold>\<forall>\<Phi> \<equiv> \<forall>x. \<A>(x) \<longrightarrow>  \<Phi>(x)"   
 abbreviation f_all_bind :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" (binder "\<^bold>\<forall>"[8]9) where "\<^bold>\<forall>x. \<phi> x \<equiv> \<^bold>\<forall>\<phi>"
  
 abbreviation f_or :: "\<sigma>\<Rightarrow>\<sigma>\<Rightarrow>\<sigma>" (infixr "\<^bold>\<or>" 51)         where "\<phi>\<^bold>\<or>\<psi> \<equiv> (\<^bold>\<not>\<phi>)\<^bold>\<rightarrow>\<psi>" 
@@ -27,8 +25,8 @@ abbreviation f_equals :: "\<iota>\<Rightarrow>\<iota>\<Rightarrow>\<sigma>" (inf
 abbreviation f_exi :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>" ("\<^bold>\<exists>")                 where "\<^bold>\<exists>\<Phi> \<equiv> \<^bold>\<not>\<^bold>\<forall>(\<lambda>y. \<not> (\<Phi> y))"
 abbreviation f_exi_b :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<sigma>"  (binder "\<^bold>\<exists>" [8]9)  where "\<^bold>\<exists>x. \<phi> x \<equiv> \<^bold>\<exists> \<phi>"
 abbreviation f_that :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<iota>" ("\<^bold>I") 
-  where "\<^bold>I \<Phi> \<equiv> if \<exists>x. \<^bold>e(x) \<and> (\<Phi> x) \<and> (\<forall>y. (\<^bold>e(x) \<and> (\<Phi> y)) \<longrightarrow> (y = x)) 
-                then THE x. \<^bold>e(x) \<and> (\<Phi> x) 
+  where "\<^bold>I \<Phi> \<equiv> if \<exists>x. \<A>(x) \<and> (\<Phi> x) \<and> (\<forall>y. (\<A>(y) \<and> (\<Phi> y)) \<longrightarrow> (y = x)) 
+                then THE x. \<A>(x) \<and> (\<Phi> x) 
                 else \<^bold>\<star>"
 abbreviation f_that_b :: "(\<iota>\<Rightarrow>\<sigma>)\<Rightarrow>\<iota>"  (binder "\<^bold>I" [8]9) where "\<^bold>I x. \<phi> x \<equiv> \<^bold>I \<phi>"
 
@@ -58,39 +56,48 @@ lemma UI_cor2 : "\<^bold>\<forall>y.((\<^bold>\<forall>x. \<^bold>\<not>(x \<^bo
 lemma UI_cor3 : "\<^bold>\<forall>y.((y \<^bold>= y) \<^bold>\<rightarrow> (\<^bold>\<exists>x. x = y))" by auto
 lemma UI_cor4 : "(\<^bold>\<forall>y. y \<^bold>= y) \<^bold>\<rightarrow> (\<^bold>\<forall>y.(\<^bold>\<exists>x. x = y))" by simp
 
-lemma "(\<^bold>\<exists>x. x = \<alpha>) \<longrightarrow> \<^bold>e(\<alpha>)" by simp
+lemma "(\<^bold>\<exists>x. x = \<alpha>) \<longrightarrow> \<A>(\<alpha>)" by simp
 -- "... to say that (\<^bold>\<exists>x. x = \<alpha>) is true means that the value of \<alpha> exists (properly" 
 
--- "Problems start here"
-lemma I1_inst : "\<^bold>\<forall>y. ((y \<^bold>= (\<^bold>Ix. \<Phi>(x))) \<^bold>\<leftrightarrow> (\<^bold>\<forall>x. ((x \<^bold>= y) \<^bold>\<leftrightarrow> \<Phi>(x))))" nitpick [user_axioms] oops
--- "There seems something wrong with my modeling of definite description!"
 
--- "This is again ok"
-lemma I2_inst : "\<^bold>\<not>(\<^bold>\<exists>y. y \<^bold>= (\<^bold>Ix. \<Phi>(x))) \<^bold>\<rightarrow>  \<^bold>\<star> = (\<^bold>Ix. \<Phi>(x))" by (metis (no_types, lifting) the_equality)
+lemma I1_inst : "\<^bold>\<forall>y. ((y \<^bold>= (\<^bold>Ix. \<Phi>(x))) \<^bold>\<leftrightarrow> (\<^bold>\<forall>x. ((x \<^bold>= y) \<^bold>\<leftrightarrow> \<Phi>(x))))" by (smt f_star_axiom the_equality)
 
--- "Extensionality: again a Problem"
-lemma Ext_inst : "(\<^bold>\<forall>x. \<Phi>(x) \<^bold>\<leftrightarrow> \<Psi>(x)) \<^bold>\<rightarrow> ((\<^bold>Ix. \<Phi>(x)) \<^bold>= (\<^bold>Ix. \<Psi>(x)))" nitpick [user_axioms] oops
+
+abbreviation star ("\<Otimes>") where "\<Otimes> \<equiv> \<^bold>Iy. \<^bold>\<not> (y \<^bold>= y)"
+
+lemma test : "\<Otimes> = \<^bold>\<star>" by simp
+
+
+lemma I2_inst : "\<^bold>\<not>(\<^bold>\<exists>y. y \<^bold>= (\<^bold>Ix. \<Phi>(x))) \<^bold>\<rightarrow>  (\<Otimes> \<^bold>= (\<^bold>Ix. \<Phi>(x)))" by (metis (no_types, lifting) the_equality)
+
+-- "Extensionality"
+lemma Ext_inst : "(\<^bold>\<forall>x. \<Phi>(x) \<^bold>\<leftrightarrow> \<Psi>(x)) \<^bold>\<rightarrow> ((\<^bold>Ix. \<Phi>(x)) \<^bold>= (\<^bold>Ix. \<Psi>(x)))" by (smt the1_equality)
 
 
 -- "the following schema is not valid as intended"
-lemma I3 : "(\<^bold>\<star> \<^bold>= \<alpha> \<^bold>\<or> \<^bold>\<star> \<^bold>= \<beta>) \<^bold>\<rightarrow> \<^bold>\<not>(\<alpha> \<^bold>r \<beta>)" nitpick [user_axioms] oops
+lemma I3 : "(\<Otimes> \<^bold>= \<alpha> \<^bold>\<or> \<Otimes> \<^bold>= \<beta>) \<^bold>\<rightarrow> \<^bold>\<not>(\<alpha> \<^bold>r \<beta>)" nitpick [user_axioms] oops
 
+
+-- "The following is not valid, Problem?"
 lemma Russel_inst : 
- "((\<^bold>\<star> \<^bold>= \<alpha> \<^bold>\<or> \<^bold>\<star> \<^bold>= \<beta>) \<^bold>\<rightarrow> \<^bold>\<not>(\<alpha> \<^bold>r \<beta>)) 
+ "((\<Otimes> \<^bold>= \<alpha> \<^bold>\<or> \<Otimes> \<^bold>= \<beta>) \<^bold>\<rightarrow> \<^bold>\<not>(\<alpha> \<^bold>r \<beta>)) 
   \<^bold>\<rightarrow> 
   ((\<alpha> \<^bold>r (\<^bold>Ix. \<Phi>(x))) \<^bold>\<leftrightarrow> (\<^bold>\<exists>y.((\<^bold>\<forall>x. ((x \<^bold>= y) \<^bold>\<leftrightarrow> \<Phi>(x))) \<^bold>\<and> (\<alpha> \<^bold>r y))))"
 nitpick [user_axioms] oops
 
 
+
+
+
 lemma "\<^bold>\<not>(\<^bold>\<exists>x. (x \<^bold>= (\<^bold>Iy. \<^bold>\<not> (y \<^bold>= y))))" using f_star_axiom by auto
-lemma "(\<^bold>\<exists>x. x \<^bold>= a) \<^bold>\<rightarrow>  \<^bold>e a" by simp
+lemma "(\<^bold>\<exists>x. x \<^bold>= a) \<^bold>\<rightarrow>  \<A>(a)" by simp
 
 
 -- "Some further test"
 
 consts a::\<iota> b::\<iota> 
-axiomatization where ax1: "\<^bold>e a \<^bold>\<and> \<^bold>e b \<^bold>\<and> \<^bold>\<not> (a \<^bold>= b) \<^bold>\<and> \<^bold>\<not> (a \<^bold>=  \<^bold>\<star>) \<^bold>\<and> \<^bold>\<not> (b \<^bold>=  \<^bold>\<star>)"
-lemma test: "\<^bold>\<star> \<^bold>= (\<^bold>I (\<lambda>x. x  \<^bold>= a \<^bold>\<or> x \<^bold>= b))" by (metis ax1)
+axiomatization where ax1: "\<A>(a) \<^bold>\<and> \<A>(b) \<^bold>\<and> \<^bold>\<not> (a \<^bold>= b) \<^bold>\<and> \<^bold>\<not> (a \<^bold>= \<Otimes>) \<^bold>\<and> \<^bold>\<not> (b \<^bold>= \<Otimes>)"
+lemma test2: "\<Otimes> \<^bold>= (\<^bold>I (\<lambda>x. x  \<^bold>= a \<^bold>\<or> x \<^bold>= b))" by (metis ax1)
 
 
 
