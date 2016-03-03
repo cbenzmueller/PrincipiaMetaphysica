5cc6038a2f64e10f74155505397df19a2e2b3454
Benzmüller <cbenzmueller@ontoleo-4.local>
2016-03-03 16:47:55 +0100

    some experiments

--- FreeLogic/Scott3_2.thy
+++ FreeLogic/Scott3_2.thy
@@ -33,7 +33,6 @@ lemma a10: "E (x\<cdot>y) \<^bold>\<rightarrow> cod(x\<cdot>y) \<approx> cod(x)"
 lemma a11: "(E(x\<cdot>y) \<^bold>\<and> E(y\<cdot>z)) \<^bold>\<rightarrow> E(x\<cdot>(y\<cdot>z))" by (metis a10 a3)
 
 
-
 text {* Important remark: The proofs seem much harder if we use Scott2.thy instead of Scott1.thy in 
  the import; that is, if we use the alternative theory of equality. In fact the provers, including 
  sledgehammer, fail to deliver proofs in this case!?! Thus the theory of equality in Scott1 seems 
@@ -53,49 +52,35 @@ lemma F1_1: "E(y\<cdot>x) \<^bold>\<leftrightarrow> cod(x) \<approx>F dom(y)" ni
 lemma F1_2: "E(y\<cdot>x) \<^bold>\<leftrightarrow> cod(x) \<approx> dom(y)"  by (metis a3) (* proof *)
 lemma F1_3: "E(y\<cdot>x) \<^bold>\<leftrightarrow> cod(x) \<^bold>\<equiv> dom(y)"  nitpick [user_axioms] oops (* countermodel *)
 
-lemma F2a_1: "cod(dom(x)) \<approx>F dom(x)" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *) 
+lemma F2a_1: "cod(dom(x)) \<approx>F dom(x)" using a7 by auto (* proof *) 
 lemma F2a_2: "cod(dom(x)) \<approx> dom(x)"  nitpick [user_axioms] oops (* countermodel *) 
 lemma F2a_3: "cod(dom(x)) \<^bold>\<equiv> dom(x)"  using a7 by force (* proof *) 
 
-lemma F2b_1: "dom(cod(x)) \<approx>F cod(x)" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *) 
+lemma F2b_1: "dom(cod(x)) \<approx>F cod(x)" using a8 by auto (* proof *) 
 lemma F2b_2: "dom(cod(x)) \<approx> cod(x)"  nitpick [user_axioms] oops (* countermodel *) 
-lemma F2b_1: "dom(cod(x)) \<^bold>\<equiv> cod(x)"  using a8 by fastforce (* proof *) 
+lemma F2b_3: "dom(cod(x)) \<^bold>\<equiv> cod(x)"  using a8 by fastforce (* proof *) 
 
-lemma F3a_1: "x\<cdot>dom(x) \<approx>F x" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *)
+lemma F3a_1: "x\<cdot>dom(x) \<approx>F x" using a5 by blast (* proof *)
 lemma F3a_2: "x\<cdot>dom(x) \<approx> x"  nitpick [user_axioms] oops (* countermodel *)
 lemma F3a_3: "x\<cdot>dom(x) \<^bold>\<equiv>  x" using a5 by blast (* proof *)
  
-lemma F3b_1: "cod(x)\<cdot>x \<approx>F x" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *) 
+lemma F3b_1: "cod(x)\<cdot>x \<approx>F x" using a6 by blast (* no countermodel, no proof *) 
 lemma F3b_2: "cod(x)\<cdot>x \<approx> x"  nitpick [user_axioms] oops (* countermodel *)
 lemma F3b_3: "cod(x)\<cdot>x \<^bold>\<equiv> x"  using a6 by blast (* proof *)
 
 lemma F4a_1: "dom(y\<cdot>x) \<approx>F dom(dom(y)\<cdot>x)" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *)  
 lemma F4a_2: "dom(y\<cdot>x) \<approx> dom(dom(y)\<cdot>x)"  nitpick [user_axioms] oops (* countermodel *) 
-lemma F4a_3: "dom(y\<cdot>x) \<^bold>\<equiv> dom(dom(y)\<cdot>x)"  by (metis F3a_3 a3 a7 a9) (* proof *)
+lemma F4a_3: "dom(y\<cdot>x) \<^bold>\<equiv> dom(dom(y)\<cdot>x)" sledgehammer nitpick [user_axioms] 
+by (metis F3a_3 a3 a7 a9) (* no countermodel, no proof *) 
 
 lemma F4b_1: "cod(y\<cdot>x) \<approx>F cod(y\<cdot>cod(x))" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *)
 lemma F4b_2: "cod(y\<cdot>x) \<approx> cod(y\<cdot>cod(x))"  nitpick [user_axioms] oops (* countermodel *)
-lemma F4b_3: "cod(y\<cdot>x) \<^bold>\<equiv> cod(y\<cdot>cod(x))"  by (metis F3b_3 a10 a2 a3) (* proof *) 
+lemma F4b_3: "cod(y\<cdot>x) \<^bold>\<equiv> cod(y\<cdot>cod(x))"  sledgehammer nitpick [user_axioms] 
+by (metis F3b_3 a10 a2 a3) (* no countermodel, no proof *)
 
 lemma FA5_1: "x\<cdot>(y\<cdot>z) \<approx>F (x\<cdot>y)\<cdot>z" sledgehammer nitpick [user_axioms] oops (* no countermodel, no proof *)
 lemma FA5_2: "x\<cdot>(y\<cdot>z) \<approx> (x\<cdot>y)\<cdot>z"  nitpick [user_axioms] oops (* countermodel *)
 lemma FA5_3: "x\<cdot>(y\<cdot>z) \<^bold>\<equiv> (x\<cdot>y)\<cdot>z"  using a4 by blast (* no proof *)
 
-lemma Freyd_1: "E(y\<cdot>x) \<^bold>\<leftrightarrow> cod(x) \<approx>F dom(y)" 
- nitpick [user_axioms,mono] oops (* counterexample, why? *)
-lemma Freyd_2a: "cod(dom(x)) \<approx>F dom(x)" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *) 
-lemma Freyd_2b: "dom(cod(x)) \<approx>F cod(x)" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *) 
-lemma Freyd_3a: "x\<cdot>dom(x) \<approx>F x" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *) 
-lemma Freyd_3b: "cod(x)\<cdot>x \<approx>F x" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *) 
-lemma Freyd_4a: "dom(y\<cdot>x) \<approx>F dom(dom(y)\<cdot>x)" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *)  
-lemma Freyd_4b: "cod(y\<cdot>x) \<approx>F cod(y\<cdot>cod(x))" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *) 
-lemma Freyd_A5: "x\<cdot>(y\<cdot>z) \<approx>F (x\<cdot>y)\<cdot>z" 
- sledgehammer nitpick [user_axioms,mono] oops (* no counterexample, but also no proof *) 
 end 
 
